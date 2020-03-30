7c12292566243cef0db42e855254a9e20ba0e248
Franco Victorio <victorio.franco@gmail.com>
2020-03-30 20:34:20 -0300

    Updates

---
+++ precache-manifest.3dd3523df9847bca02952ea3461256dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa948abba1c4406f44bf8766c90af97e",
+    "url": "/cpk-address/index.html"
+  },
+  {
+    "revision": "3cee66be63f71ee869e5",
+    "url": "/cpk-address/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b9a124ba3c74ba31d0d5",
+    "url": "/cpk-address/static/js/2.a849416a.chunk.js"
+  },
+  {
+    "revision": "8fa686e03e426885d9bffc3697b95bbe",
+    "url": "/cpk-address/static/js/2.a849416a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cee66be63f71ee869e5",
+    "url": "/cpk-address/static/js/main.465dc9a9.chunk.js"
+  },
+  {
+    "revision": "1a426528c431ac8f2901",
+    "url": "/cpk-address/static/js/runtime-main.40ed164d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c5cdf58b89bc14dfc8aafde0778b419b.js"
+  "/cpk-address/precache-manifest.3dd3523df9847bca02952ea3461256dd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cpk-address/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
